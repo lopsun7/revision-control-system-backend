84b1e58b03be8cbda62d72af7e6842ea2297b047
Shuaqi Zeng <szeng@scu.edu>
2024-05-23 17:19:29 -0700

    initial demo 0.26 runnable main（still issues with raft node configuration）

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hashicorp/raft"
+	"log"
+	"strconv"
+	"time"
+)
 
 func main() {
-	fmt.Println(111)
+	nodeCount := 3
+	raftPortStart := 5000
+	nodes := make([]*raft.Raft, nodeCount)
+	transports := make([]raft.Transport, nodeCount)
+	ids := make([]raft.ServerID, nodeCount)
+
+	for i := 0; i < nodeCount; i++ {
+		nodeID := "node" + strconv.Itoa(i+1)
+		raftPort := raftPortStart + i
+		nodes[i], transports[i], ids[i] = setupRaft(nodeID, raftPort)
+		time.Sleep(1 * time.Second) // Ensure nodes have time to start properly
+	}
+
+	// Bootstrap the cluster with the first node
+	config := raft.Configuration{
+		Servers: []raft.Server{
+			{
+				ID:      ids[0],
+				Address: transports[0].LocalAddr(),
+			},
+		},
+	}
+	future := nodes[0].BootstrapCluster(config)
+	if err := future.Error(); err != nil {
+		log.Fatalf("Failed to bootstrap cluster: %v", err)
+	}
+
+	// Wait until the first node becomes the leader
+	time.Sleep(5 * time.Second)
+
+	// Have the other nodes join the cluster
+	for i := 1; i < nodeCount; i++ {
+		addVoterFuture := nodes[0].AddVoter(ids[i], transports[i].LocalAddr(), 0, 0)
+		if err := addVoterFuture.Error(); err != nil {
+			log.Fatalf("Failed to add voter for node %d: %v", i, err)
+		}
+	}
+
+	log.Println("Raft cluster initialized and running")
+	select {} // Keep the main goroutine running
 }
--- raft_cluster.go
+++ raft_cluster.go
@@ -1,31 +1,41 @@
 package main
 
 import (
-	"github.com/hashicorp/raft"
-	"github.com/hashicorp/raft-boltdb"
+	"log"
 	"net"
 	"os"
 	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft-boltdb"
 )
 
-func setupRaft(nodeID string, raftPort int) (*raft.Raft, *FileStore) {
+func setupRaft(nodeID string, raftPort int) (*raft.Raft, raft.Transport, raft.ServerID) {
+	// Define base directory for this node
 	baseDir := filepath.Join("tmp", "raft", nodeID)
 	os.MkdirAll(baseDir, 0700)
 
+	// Setup Raft configuration
 	config := raft.DefaultConfig()
 	config.LocalID = raft.ServerID(nodeID)
 
+	// Setup network address and transport
 	address := "127.0.0.1:" + strconv.Itoa(raftPort)
 	addr, _ := net.ResolveTCPAddr("tcp", address)
 	transport, _ := raft.NewTCPTransport(address, addr, 3, 10*time.Second, os.Stderr)
 
+	// Setup log store, stable store, and snapshot store
 	logStore, _ := raftboltdb.NewBoltStore(filepath.Join(baseDir, "raft.db"))
 	stableStore, _ := raftboltdb.NewBoltStore(filepath.Join(baseDir, "stable.db"))
 	snapshotStore, _ := raft.NewFileSnapshotStore(baseDir, 1, os.Stderr)
 
-	fsm := NewFileStore()
-	ra, _ := raft.NewRaft(config, fsm, logStore, stableStore, snapshotStore, transport)
-	return ra, fsm
+	// Create Raft node
+	node, err := raft.NewRaft(config, nil, logStore, stableStore, snapshotStore, transport)
+	if err != nil {
+		log.Fatalf("Failed to create Raft node: %v", err)
+	}
+
+	return node, transport, raft.ServerID(nodeID)
 }
